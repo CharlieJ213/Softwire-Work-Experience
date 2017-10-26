9a01c076b3467a1e45414d62e707b10c9d8e23f3
CharlieJ213 <charlie.jxcques@gmail.com>
2017-10-26 14:37:52 +0100

    Prototype Design and QuestionsAndAnswers Object Built

---
+++ script.js
@@ -0,0 +1,38 @@
+//Object filled with Questions and answers for each section. The first answer for every Array of answers is the correct answer.
+let QsAndAs = {
+	"Section1": {
+		"Question": "What is the Song and Artist name of the following?",
+		"Source": ["Question1Audio.wav", "Question2Audio.wav", "Question3Audio.wav"],
+		"CorrectAns": ["Blind Faith - Chase and Status", "Have A Nice Day - Stereophonics", "Grounds for Divorce - Elbow"],
+		"Answers": {
+			"Q1": ["The Rockafeller Skank - Fat Boy Slim", "Too Close - Alex Clare", "Turn Down For What - DJ Snake"],
+			"Q2": ["Radioactive - Imagine Dragons", "Bohemian Like You - The Dandy Warhols", "Hostage - Nothing But Theives"],
+			"Q3": ["My Iron Lung - Radiohead", "Black - Pearl Jam", "Dark Necessities - Red Hot Chilli Peppers"],
+		}
+	},
+	"Section2": {
+		"Questions": ["Who did Public Enemy mainly sample in their track 'Harder Than You Think'?", "Who did Kanye West sample in his track 'Touch the Sky'?", "Who did The Beatles cover with their track 'Twist and Shout'?"],
+		"CorrectAns": ["Shirley Bassey", "Curtis Mayfield", "Isley Brothers"],
+		"Answers": {
+			"Q1": ["Jerry-Lee Lewis", "Otis Redding", "Fat Boy Slim"],
+			"Q2": ["Al Green", "James Brown", "Marvin Gaye"],
+			"Q3": ["Beach Boys", "Bob Dylan", "Chuck Berry"],
+		}
+	},
+	"Section3": {
+		"Questions": ["Who headlined the last night of the Reading Festival 2017?", "Why could the Foo Fighters not play Glastonbury 2015?"],
+		"CorrectAns": ["Muse", "Lead singer broke his leg"],
+		"Answers": {
+			"Q1": ["Queens of the Stone Age", "Kasabian", "Eminem"],
+			"Q2": ["Their flights were cancelled", "They didn't want to", "Personal issues for one of the members"],
+	},
+	"Section4": {
+		"Questions": ["Where in America did The Black Keys originate?", "Where in the UK did the Arctic Monkeys originate?"],
+		"CorrectAns": ["Seattle", "Sheffield"],
+		"Answers": {
+			"Q1": ["Seattle", "New York", "Los Angeles", "Wisconsin"],
+			"Q2": ["Sheffield", "Manchester", "London", "Cardiff"],
+		}
+	},
+}
+}
